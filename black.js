c29f0dbe1d972590618b53230940c8cb13919045
Glitch (alabaster-cool-friday) <none>
2022-02-24 13:00:32 +0000

    ☕️🌆 Checkpoint ./black.js:26500912/2076 ./antiwebhook.json:26500912/4

--- black.js
+++ black.js
@@ -1761,3 +1761,74 @@ client.on("message", professor => {
 /////
 
 const { lineReply } = require("discord-reply"); 
+
+client.on("message", message => {
+  if (message.content.startsWith(prefix + "lock")) {
+    if (cooldown.has(message.author.id)) {
+      return message.channel.send(`You have to wait 5 seconds`).then(m => {
+        m.delete({ timeout: cdtime * 600 });
+      });
+    }
+    cooldown.add(message.author.id);
+    setTimeout(() => {
+      cooldown.delete(message.author.id);
+    }, cdtime * 1000);
+    if (!message.guild.member(message.author).hasPermission("MANAGE_CHANNELS"))
+      return message.channel.send(
+        "**You must have a higher role use this command**"
+      );
+    message.channel
+      .createOverwrite(message.guild.id, { SEND_MESSAGES: false })
+      .then(() => {
+        const embed = new Discord.MessageEmbed()
+          .setDescription(
+            `
+🔒 **channel has been locked.**
+
+Channel: <#${message.channel.id}>
+Moderator: <@${message.author.id}>
+
+**Reason**
+Not-Provided
+          `
+          )
+          .setColor(col);
+        return message.channel.send(embed);
+      });
+  }
+});
+client.on("message", message => {
+  if (message.content.startsWith(prefix + "unlock")) {
+    if (cooldown.has(message.author.id)) {
+      return message.channel.send(`You have to wait 5 seconds`).then(m => {
+        m.delete({ timeout: cdtime * 600 });
+      });
+    }
+    cooldown.add(message.author.id);
+    setTimeout(() => {
+      cooldown.delete(message.author.id);
+    }, cdtime * 1000);
+    if (!message.member.hasPermission("MANAGE_CHANNELS"))
+      return message.channel.send(
+        "**You must have a higher role use this command**"
+      );
+    message.channel
+      .createOverwrite(message.guild.id, { SEND_MESSAGES: true })
+      .then(() => {
+        const embed = new Discord.MessageEmbed()
+          .setDescription(
+            `
+🔒 **channel has been unloked.**
+Channel: <#${message.channel.id}>
+Moderator: <@${message.author.id}>
+
+**Reason**
+Not-Provided
+          
+          `
+          )
+          .setColor(col);
+        return message.channel.send(embed);
+      });
+  }
+});
